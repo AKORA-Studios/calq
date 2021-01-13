d84a82d2fd5b5801cec9602ce4e1a879e447c3fa
Kiara <frida.shivaa@gmail.com>
2021-01-13 21:29:01 +0100

    F

--- src/pages/LineChart/index.tsx
+++ src/pages/LineChart/index.tsx
@@ -4,11 +4,13 @@ import { LineChart, XAxis, Tooltip, CartesianGrid, Line, YAxis, AreaChart, Area 
 const data = [{ uv: 15 }, { uv: 2 }, { uv: 13 }, { uv: 6 }];
 const data2 = [{ uv: 12 }, { uv: 4 }, { uv: 9 }, { uv: 10 }];
 const DataSammlung = [{ name: 'Subject A', entr: data, color: '#5476f0' }, { name: 'Subject B', entr: data2, color: '#f52525' }]
+const dataNodes = [{ name: 'Test UTF8', date: '12.12.12' }, { name: 'Test Soziale Frage', date: '12.12.12' }, { name: 'Test Rekursion', date: '12.12.12' }]
 
 export class LineChartThing extends Component {
     render() {
         return (
             <div>
+
                 { (DataSammlung.map(e => {
                     return <div className="subjectDiv" style={{ display: 'grid', gridTemplateColumns: "50% 50%" }}>
                         <div>
@@ -19,30 +21,30 @@ export class LineChartThing extends Component {
                                 <XAxis dataKey="name" />
                                 <YAxis type="number" domain={[0, 15]} />
                             </LineChart>
-
-
                         </div>
 
-                        <h1 className="subjectDescription" style={{ color: e.color }}>dfsdf</h1>
-                        {/*
-                        <AreaChart
-                            width={500}
-                            height={400}
-                            data={data}
-                            margin={{
-                                top: 10, right: 30, left: 0, bottom: 0,
-                            }}
-                        >
-                            <CartesianGrid />
-                            <XAxis dataKey="name" />
-                            <YAxis />
-                            <Tooltip />
-                            <Area type="monotone" dataKey="uv" stroke="#ed8b13" fill="#ed8b13" />
-                        </AreaChart>
-                        */}
+
+                        <div>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            {(dataNodes.map(d => {
+                                return <div className="subjectTestInfo" style={{ display: 'grid', gridTemplateColumns: "50% 50%" }}>
+                                    <h1 className="subjectDescription" style={{ color: e.color }}>{d.name}</h1>
+                                    <div style={{
+                                        borderColor: 'white', borderLeftWidth: '1px', borderLeftStyle: 'solid'
+                                    }}>
+                                        <h1 className="subjectDescription" style={{ color: "white" }}>{d.date}</h1>
+                                    </div>
+
+                                </div>
+                            }))}
+                        </div>
                     </div>
                 }))}
                 <br></br>
+
 
             </div>
         )
