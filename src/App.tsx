471a62b23d7c80ba60b60885e788394ecb461ec3
Kiara <frida.shivaa@gmail.com>
2021-01-13 20:35:40 +0100

    🍒🍉🍈

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { MainPage, LineChartThing } from './pages'
+import { Navbar } from './Components'
 import { grey } from './colors.json'
 
 function App() {
     return (
         <div className="App">
-            <MainPage></MainPage>
+            <Navbar></Navbar>
             <LineChartThing></LineChartThing>
         </div>
+
     );
 }
 
---
+++ src/Components/NavbarComponent/index.tsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import './navbar.css'
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <div className="navbar">
+                <h1 className="headline">Navbar Test</h1>
+
+            </div>
+        )
+    }
+}
