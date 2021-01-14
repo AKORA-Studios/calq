b3b9d12695e997f4c5edd3ed709b4b88c77da6ad
Kiara <frida.shivaa@gmail.com>
2021-01-14 21:26:09 +0100

    Edited navbar

--- src/Components/NavbarComponent/index.tsx
+++ src/Components/NavbarComponent/index.tsx
@@ -8,11 +8,12 @@ const overview = () => window.location.href = 'http://localhost:3000/overview';
 export class Navbar extends Component {
     render() {
         return (
-            <div className="navbar" style={{ display: 'grid', gridTemplateColumns: "15% 15% 55% 15%", width: "100%", color: '#ed8b13', height: '4.5rem' }} >
-                <div className="navbarButton" onClick={home}> <p className="navbarButtonText"> HOME </p></div>
-                <div className="navbarButton" onClick={overview}> <p className="navbarButtonText"> OVERVIEW </p></div>
+            <div className="navbar" style={{ display: 'grid', gridTemplateColumns: "15% 15% 40% 15% 15%", width: "100%", color: '#ed8b13', height: '4.5rem' }} >
+                <div className="navbarButton" onClick={home}> <p className="navbarButtonText">✉ HOME</p></div>
+                <div className="navbarButton" onClick={overview}> <p className="navbarButtonText">OVERVIEW </p></div>
                 <div className="navbarButtonTitle">  <h1 className="headline">NAVIGATION BAR</h1></div>
-                <div className="navbarButton" onClick={nothing}> <p className="navbarButtonText"> LOGIN </p></div>
+                <div className="navbarButton" onClick={nothing}> <p className="navbarButtonText">✚ ADD</p></div>
+                <div className="navbarButton" onClick={nothing}> <p className="navbarButtonText">LOGIN</p></div>
             </div>
         )
     }
