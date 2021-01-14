f25f6219993fde4054b865681261b88410a419f0
Kiara <frida.shivaa@gmail.com>
2021-01-14 20:58:55 +0100

    F

--- src/Components/NavbarComponent/index.tsx
+++ src/Components/NavbarComponent/index.tsx
@@ -11,7 +11,7 @@ export class Navbar extends Component {
             <div className="navbar" style={{ display: 'grid', gridTemplateColumns: "15% 15% 55% 15%", width: "100%", color: '#ed8b13', height: '4.5rem' }} >
                 <div className="navbarButton" onClick={home}> <p className="navbarButtonText"> HOME </p></div>
                 <div className="navbarButton" onClick={overview}> <p className="navbarButtonText"> OVERVIEW </p></div>
-                <div className="navbarButton">  <h1 className="headline">NAVIGATION BAR</h1></div>
+                <div className="navbarButtonTitle">  <h1 className="headline">NAVIGATION BAR</h1></div>
                 <div className="navbarButton" onClick={nothing}> <p className="navbarButtonText"> LOGIN </p></div>
             </div>
         )
