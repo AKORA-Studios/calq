3258cda86e638d18695da91b14a547d4cce080ea
DragonCat || Kiara || Yumi <68660034+DragonCat4012@users.noreply.github.com>
2021-01-15 17:16:35 +0100

    added addSubject Method

--- src/database/api.ts
+++ src/database/api.ts
@@ -5,3 +5,11 @@ const url = "http://localhost:3001/api/";
 export function getSubjects(): Promise<Subject[]> {
     return fetch(url + "subjects").then(r => r.json())
 }
+
+/** Returns the Status Code */
+export function addSubject(data: Subject): Promise<number> {
+    return fetch(url + "subjects", {
+        body: JSON.stringify(data),
+        method: 'POST'
+    }).then(r => r.status);
+}
