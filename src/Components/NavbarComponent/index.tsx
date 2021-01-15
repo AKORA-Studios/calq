b96db67bf7d6c866ae2f1f7bd94fdc04ab2bacab
DragonCat || Kiara || Yumi <68660034+DragonCat4012@users.noreply.github.com>
2021-01-15 12:42:44 +0100

    Switched to Links

--- src/Components/NavbarComponent/index.tsx
+++ src/Components/NavbarComponent/index.tsx
@@ -1,9 +1,6 @@
 import { Component } from "react";
+import { Link } from "react-router-dom";
 import '../components.css';
-
-const home = () => window.location.href = 'http://localhost:3000/';
-const nothing = () => { }
-const overview = () => window.location.href = 'http://localhost:3000/overview';
 
 interface Props {
     type: string
@@ -14,11 +11,11 @@ export class Navbar extends Component<Props> {
     render() {
         return (
             <div className="navbar" style={{ display: 'grid', gridTemplateColumns: "15% 15% 40% 15% 15%", width: "100%", color: '#ed8b13', height: '4.5rem' }} >
-                <div className="navbarButton" onClick={home}> <p className={this.props.type === 'main' ? "activeNavbarButtonText" : 'navbarButtonText'}>✉ HOME</p></div>
-                <div className="navbarButton" onClick={overview}> <p className={this.props.type === 'overview' ? "activeNavbarButtonText" : 'navbarButtonText'}>OVERVIEW </p></div>
+                <Link to="/" className="navbarButton"> <p className={this.props.type === 'main' ? "activeNavbarButtonText" : 'navbarButtonText'}>✉ HOME</p></Link>
+                <Link to="/overview" className="navbarButton"> <p className={this.props.type === 'overview' ? "activeNavbarButtonText" : 'navbarButtonText'}>OVERVIEW </p></Link>
                 <div className="navbarButtonTitle">  <h1 className="headline">NAVIGATION BAR</h1></div>
-                <div className="navbarButton" onClick={nothing}> <p className="navbarButtonText">✚ ADD</p></div>
-                <div className="navbarButton" onClick={nothing}> <p className="navbarButtonText">LOGIN</p></div>
+                <Link to="/" className="navbarButton" > <p className="navbarButtonText">✚ ADD</p></Link>
+                <Link to="/" className="navbarButton" > <p className="navbarButtonText">LOGIN</p></Link>
             </div>
         )
     }
