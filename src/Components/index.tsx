d844e2c0b501a3a3ccb81a9e2b1ca18bbf0d5dc5
DragonCat || Kiara || Yumi <68660034+DragonCat4012@users.noreply.github.com>
2021-01-16 21:40:41 +0100

    rename

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -1,4 +1,4 @@
-export { SubjectBarChartAll as SubejctBarChartAll } from './SubjectBarChart'
+export { SubjectBarChartAll } from './SubjectBarChart'
 export { RadialChartAll } from './RadialChartAll'
 export * from './SubjectComponent'
 export * from './PageComponents'
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,11 +1,11 @@
 import { Component } from "react";
-import { Page, RadialChartAll, SubejctBarChartAll } from '../../Components';
+import { Page, RadialChartAll, SubjectBarChartAll } from '../../Components';
 
 export class MainPage extends Component {
     render() {
         return (
             <Page name="home" style={{ display: 'grid', gridTemplateColumns: "50% 50%" }}>
-                <SubejctBarChartAll />
+                <SubjectBarChartAll />
                 <RadialChartAll />
             </Page>
         )
