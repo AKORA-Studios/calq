8b906924c36ee54ebf4c3e8df05c9e96978b1f7e
DragonCat || Kiara || Yumi <68660034+DragonCat4012@users.noreply.github.com>
2021-01-17 19:19:30 +0100

    Added Login and Reister

--- src/pages/LogInPage/index.tsx
+++ src/pages/LogInPage/index.tsx
@@ -1,42 +1,27 @@
-import { Box, Button, ButtonGroup, Center, FormControl, FormErrorMessage, FormLabel, Input, Radio } from "@chakra-ui/react";
-import { Field, Form, Formik } from "formik";
-import { CheckboxContainer, CheckboxControl, CheckboxSingleControl, InputControl, NumberInputControl, PercentComplete, RadioGroupControl, ResetButton, SelectControl, SubmitButton, SwitchControl, TextareaControl } from "formik-chakra-ui";
+import { Box, Button, Center, Grid, } from "@chakra-ui/react";
+import { Formik } from "formik";
+import { InputControl, SubmitButton } from "formik-chakra-ui";
 import { Component } from "react";
+import { Link } from "react-router-dom";
+import * as Yup from 'yup';
 import { Page } from '../../Components';
 
-interface FormValues {
-    firstName: string;
-}
+const validationSchema = Yup.object({
+    username: Yup.string().required(),
+    password: Yup.string().required(),
+});
+
+
 
 export class LoginPage extends Component {
     initialValues = {
-        firstName: "",
-        lastName: "",
-        age: 0,
-        employed: false,
-        favoriteColor: "",
-        toppings: ["tuna"],
-        notes: "",
-        employedd: false
+
     };
 
-    sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
-    onSubmit = (values: {}) => {
-        this.sleep(300).then(() => {
-            window.alert(JSON.stringify(values, null, 2));
-        });
-    };
-
-    validateName(value: string) {
-        let error
-        if (!value) {
-            error = "Name is required"
-        } else if (value.toLowerCase() !== "naruto") {
-            error = "Jeez! You're not a fan 😱"
-        }
-        return error
+    onSubmit(values: {}) {
+        window.alert(JSON.stringify(values, null, 4));
     }
+
 
 
     render() {
@@ -46,7 +31,8 @@ export class LoginPage extends Component {
                     <Formik
                         initialValues={this.initialValues}
                         onSubmit={this.onSubmit}
-                    >
+                        validationSchema={validationSchema}>
+
                         {({ handleSubmit, values, errors }) => (
                             <Box
                                 borderWidth="1px"
@@ -55,62 +41,16 @@ export class LoginPage extends Component {
                                 maxWidth={800}
                                 p={6}
                                 m="10px auto"
+                                pt="5px"
                                 as="form"
-                                onSubmit={handleSubmit as any}
-                            >
-                                <InputControl name="firstName" label="First Name" />
-                                <InputControl name="lastName" label="Last Name" />
-                                <NumberInputControl name="age" label="Last Name" />
-                                <CheckboxSingleControl name="employed">
-                                    Employed
-                                </CheckboxSingleControl>
-                                <RadioGroupControl name="favoriteColor" label="Favorite Color">
-                                    <Radio value="#ff0000">Red</Radio>
-                                    <Radio value="#00ff00">Green</Radio>
-                                    <Radio value="#0000ff">Blue</Radio>
-                                </RadioGroupControl>
-                                <CheckboxContainer name="toppings" label="Toppings">
-                                    <CheckboxControl name="toppings" value="chicken">
-                                        🐓 Chicken
-                                    </CheckboxControl>
-                                    <CheckboxControl name="toppings" value="ham">
-                                        🐷 Ham
-                                    </CheckboxControl>
-                                    <CheckboxControl name="toppings" value="mushrooms">
-                                        🍄 Mushrooms
-                                    </CheckboxControl>
-                                    <CheckboxControl name="toppings" value="cheese">
-                                        🧀 Cheese
-                                    </CheckboxControl>
-                                    <CheckboxControl name="toppings" value="tuna">
-                                        🐟 Tuna
-                                    </CheckboxControl>
-                                    <CheckboxControl name="toppings" value="pineapple">
-                                        🍍 Pineapple
-                                    </CheckboxControl>
-                                </CheckboxContainer>
-                                <TextareaControl name="notes" label="Notes" />
-                                <SwitchControl name="employedd" label="Employed" />
-                                <SelectControl
-                                    name="notes"
-                                    selectProps={{ placeholder: "Select option" }}
-                                >
-                                    <option value="option1">Option 1</option>
-                                    <option value="option2">Option 2</option>
-                                    <option value="option3">Option 3</option>
-                                </SelectControl>
+                                onSubmit={handleSubmit as any}>
 
-                                <PercentComplete />
-                                <ButtonGroup>
-                                    <SubmitButton>Submit</SubmitButton>
-                                    <ResetButton>Reset</ResetButton>
-                                </ButtonGroup>
-
-                                <Box as="pre" marginY={10}>
-                                    {JSON.stringify(values, null, 2)}
-                                    <br />
-                                    {JSON.stringify(errors, null, 2)}
-                                </Box>
+                                <InputControl name="username" label="Username" />
+                                <InputControl name="password" label="Password" />
+                                <Grid>
+                                    <SubmitButton>Login</SubmitButton>
+                                    <Button as="a" href="/register" colorScheme="teal" backgroundColor="transparent" mt="3%">Register</Button>
+                                </Grid>
                             </Box>
                         )}
                     </Formik>
