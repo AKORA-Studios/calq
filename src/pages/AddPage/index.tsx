2cb312b9249268862d619a5936fa44d7b461a584
Kiara <frida.shivaa@gmail.com>
2021-01-17 16:44:07 +0100

    h

--- src/pages/AddPage/index.tsx
+++ src/pages/AddPage/index.tsx
@@ -5,10 +5,10 @@ export class AddPage extends Component {
     render() {
         return (
             <Page name="add" style={{ display: 'grid', gridTemplateColumns: "50% 50%", marginTop: '5%' }}>
-                <div style={{ backgroundColor: '#242424', marginRight: '5%', marginLeft: '5%', borderRadius: '9%' }}>
+                <div style={{ backgroundColor: '#242424', marginRight: '5%', marginLeft: '5%', borderRadius: '9%', color: 'white' }}>
                     <h1>sdfsdfsdf</h1>
                 </div>
-                <div style={{ backgroundColor: '#242424', marginRight: '5%', marginLeft: '5%', borderRadius: '9%' }}>
+                <div style={{ backgroundColor: '#242424', marginRight: '5%', marginLeft: '5%', borderRadius: '9%', color: 'white' }}>
                     <h1>gfhfghfgh</h1>
                 </div>
 
